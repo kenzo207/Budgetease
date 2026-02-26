dd62c14225c489cc8f4e2e286f7c6127f49ab3db
kenzo207 <kenzo.hounkpe@epitech.eu>
2026-02-26 22:48:06 +0100

    refactor: clean repository structure - remove legacy and obsolete files

--- budgetease_legacy/lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-const List<String> paymentMethods = ['MoMo', 'Cash', 'Carte'];
-
-const List<Map<String, String>> currencyOptions = [
-  {'value': 'FCFA', 'label': 'FCFA (Franc CFA)'},
-  {'value': 'NGN', 'label': 'NGN (Naira)'},
-  {'value': 'GHS', 'label': 'GHS (Cedi)'},
-  {'value': 'USD', 'label': 'USD (Dollar)'},
-  {'value': 'EUR', 'label': 'EUR (Euro)'},
-];
